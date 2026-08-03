93e65e4f50238e313ff99dc9bf5eabcd22fa4dcd
andpetronio <andpetronio@gmail.com>
2026-08-03 08:42:33 -0300

    updated registration period

--- form-config.js
+++ form-config.js
@@ -1,11 +1,11 @@
 (function exposeFormConfig(globalScope) {
   const formConfig = {
-    startsAt: "2026-08-05T00:00:00-03:00",
+    startsAt: "2026-08-03T00:00:00-03:00",
     expiresAt: "2026-08-15T23:59:59-03:00",
     notStartedMessage:
-      "El período de inscripción comienza el 5 de agosto de 2026 a las 00:00 h.",
+      "El período de inscripción comienza el 5 de agosto de 2026",
     expiredMessage:
-      "El período de inscripción finalizó el 15 de agosto de 2026 a las 23:59 h.",
+      "El período de inscripción finalizó el 15 de agosto de 2026",
   };
 
   if (globalScope) {
